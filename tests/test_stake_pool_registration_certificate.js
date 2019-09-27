d3e373f60ef2cb2094149b7636b991e231b6aab9
Enzo Cioppettini <ecioppettini@atixlabs.com>
2019-09-27 13:13:30 -0300

    update stake pool registration webpack test

--- tests/test_stake_pool_registration_certificate.js
+++ tests/test_stake_pool_registration_certificate.js
@@ -4,14 +4,15 @@ const rust = import('../pkg/js_chain_libs');
 
 it('generates certificate', async () => {
   const {
-    StakePoolInfo,
+    PoolRegistration,
     U128,
     Certificate,
     PrivateKey,
     PublicKey,
     KesPublicKey,
     VrfPublicKey,
-    PublicKeys
+    PublicKeys,
+    TimeOffsetSeconds
   } = await rust;
   const serial = U128.from_str('1010101010');
   const owners = new PublicKeys();
@@ -26,22 +27,25 @@ it('generates certificate', async () => {
   const vrfPublicKey = VrfPublicKey.from_bech32(
     'vrf_pk1fz79zuzm2k8agqs7a5fgdpzprgpnzh58jhz35wjftd0km8dagfdqs8e08k'
   );
-  const stakePoolInfo = new StakePoolInfo(
+  const managementThreshold = 1;
+  const startValidity = TimeOffsetSeconds.from_string('0');
+
+  const poolRegistration = new PoolRegistration(
     serial,
     owners,
+    managementThreshold,
+    startValidity,
     kesPublicKey,
     vrfPublicKey
   );
-  expect(stakePoolInfo.id().to_string()).to.eql(
-    'b86471bd72599a45a90fe2cdf4bdc0cc8a8c9af9e87521b64e821ca7d99c2993'
+
+  expect(poolRegistration.id().to_string()).to.eql(
+    '6d7afbf4a8e0574a5dbbf35a71a42da86ebf679d864a03153446a4e671b26edc'
   );
-  const certificate = Certificate.stake_pool_registration(stakePoolInfo);
+  const certificate = Certificate.stake_pool_registration(poolRegistration);
   certificate.sign(
     PrivateKey.from_bech32(
       'ed25519_sk1mr52spjelqhs5hegxuymh8la04gkrxh5543cf4c7hx8x3mz3sadqdfdx5y'
     )
-  );
-  expect(certificate.to_bech32()).to.eql(
-    'cert1qgqqqqqqqqqqqqqqqqqqq0p5avfqrk4hkkn4z57flzytw5v4r0wvh6ua3cmvzs9xsz4gaw5xzjjmyw3ks6pr56t6uzkmgdqs2krrh0tw9yyvdwfkzk7e90nsfxg08wqxg6q530z3wpd4tr75qg0w6y5xs3q35qe3t6ret3g68fy4khmdnk75ykspqpqfc2knaxwmgkzenlvmewe77y04uzlvlemqqvtd7xceftqwvkx8483vmaz2086fvrhs5wddj6s5ptpdx7dfljwyw94fm4m20e9wkqk3pg3d2n39'
   );
 });
