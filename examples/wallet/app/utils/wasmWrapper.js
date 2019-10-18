c1215fcb0a4f9ea414d3535d320176dc67b054f0
Juan Pablo Capurro <jpcapurro@atixlabs.com>
2019-10-18 12:24:52 -0300

    generating a hopefully valid transaction

--- examples/wallet/app/utils/wasmWrapper.js
+++ examples/wallet/app/utils/wasmWrapper.js
@@ -32,6 +32,7 @@ export async function buildTransaction(
   destination: string,
   amount: number,
   secret: string,
+  accountCounter: number,
   nodeSettings: NodeSettings
 ): Promise<Uint8Array> {
   const {
@@ -42,8 +43,10 @@ export async function buildTransaction(
     Value,
     Fee,
     TransactionFinalizer,
+    Transaction,
     Fragment,
     PrivateKey,
+    PublicKey,
     Witness,
     SpendingCounter,
     Hash,
@@ -52,16 +55,44 @@ export async function buildTransaction(
   const privateKey: PrivateKey = PrivateKey.from_bech32(secret);
   const sourcePublicKey: PublicKey = privateKey.to_public();
   const sourceAccount: Account = Account.from_public_key(sourcePublicKey);
-  // FIXME: make the discrimination configurable
-  // FIXME somebody please tell me how do we determine the prefix
-  // and if it needs to be configurable
-  const address: string = account
-    .to_address(AddressDiscrimination.Test)
-    .to_string('ca');
+
+  // Fee (#inputs + #outputs) * coefficient + constant + #certificates*certificate
+  // #inputs = 1; #outputs = 2; #certificates = 0
+  const computedFee =
+    (1 + 1) * nodeSettings.fees.coefficient + nodeSettings.fees.constant;
+  const inputAmount = computedFee + amount;
+  const input: Input = Input.from_account(
+    sourceAccount,
+    Value.from_str(inputAmount.toString())
+  );
   const txbuilder: TransactionBuilder = TransactionBuilder.new_no_payload();
-  return {
-    address,
-    privateKey: secret,
-    identifier: identifier.to_hex()
-  };
+  txbuilder.add_input(input);
+
+  txbuilder.add_output(
+    Address.from_string(destination),
+    Value.from_str(amount.toString())
+  );
+
+  const feeAlgorithm = Fee.linear_fee(
+    Value.from_str(nodeSettings.fees.constant.toString()),
+    Value.from_str(nodeSettings.fees.coefficient.toString()),
+    Value.from_str(nodeSettings.fees.certificate.toString())
+  );
+  // The amount is exact, that's why we use `forget()`
+  const finalizedTx: Transaction = txbuilder.seal_with_output_policy(
+    feeAlgorithm,
+    OutputPolicy.forget()
+  );
+  const finalizer: TransactionFinalizer = new TransactionFinalizer(finalizedTx);
+
+  const witness = Witness.for_account(
+    Hash.from_hex(nodeSettings.block0Hash),
+    finalizer.get_tx_sign_data_hash(),
+    privateKey,
+    SpendingCounter.from_u32(accountCounter)
+  );
+  finalizer.set_witness(0, witness);
+  const signedTx = finalizer.finalize();
+  const message: Fragment = Fragment.from_authenticated_transaction(signedTx);
+  return message.as_bytes();
 }
