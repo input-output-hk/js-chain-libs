e883bcc8530baa5dafb3c888a56d6c9321f9fe11
Juan Pablo Capurro <jpcapurro@atixlabs.com>
2019-10-21 15:15:19 -0300

    added a manual refresh button

--- examples/wallet/app/components/Home.js
+++ examples/wallet/app/components/Home.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import AccountInfo from '../containers/AccountInfo';
 import SendTransaction from '../containers/SendTransaction';
+import RefreshBalance from '../containers/RefreshBalance';
 
 export default () => {
   return (
     <div>
       <h2> Account Info</h2>
       <AccountInfo />
+      <RefreshBalance />
       <hr />
       <h2> Send funds</h2>
       <SendTransaction />
