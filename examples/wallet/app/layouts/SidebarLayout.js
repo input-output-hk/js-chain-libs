ce7d9fcbe54ab84f1f99bef28665dd34b7043196
Juan Pablo Capurro <jpcapurro@atixlabs.com>
2019-11-01 11:27:03 -0300

    the sidebar now lets the user navigate to other (mocked) screens

--- examples/wallet/app/layouts/SidebarLayout.js
+++ examples/wallet/app/layouts/SidebarLayout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import type { Node } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import SVGInline from 'react-svg-inline';
+import { useHistory } from 'react-router-dom';
+import ClickableBox from 'clickable-box';
 import styles from './SidebarLayout.scss';
 import walletIcon from '../assets/images/wallet-ic.inline.svg';
 import sendIcon from '../assets/images/send-ic.inline.svg';
 import settingsIcon from '../assets/images/settings-ic.inline.svg';
 import delegationIcon from '../assets/images/delegation-ic.inline.svg';
+import routes from '../constants/routes.json';
 
 type Props = {
   children: Node
@@ -24,27 +27,43 @@ export default ({ children }: Props) => {
 
 const SidebarContent = () => (
   <Nav className={`flex-column justify-content-between ${styles.nav}`}>
-    <Nav.Item>
-      <ResponsiveSidebarItem icon={walletIcon} text="wallet" />
-      <ResponsiveSidebarItem active icon={sendIcon} text="send" />
-      <ResponsiveSidebarItem icon={delegationIcon} text="delegate" />
-    </Nav.Item>
-    <Nav.Item>
-      <ResponsiveSidebarItem icon={settingsIcon} text="settings" />
-    </Nav.Item>
+    <div>
+      <ResponsiveSidebarItem
+        route={routes.HOME}
+        icon={walletIcon}
+        text="wallet"
+      />
+      <ResponsiveSidebarItem route={routes.SEND} icon={sendIcon} text="send" />
+      <ResponsiveSidebarItem
+        route={routes.STAKING}
+        icon={delegationIcon}
+        text="delegate"
+      />
+    </div>
+    <ResponsiveSidebarItem
+      route={routes.SETTINGS}
+      icon={settingsIcon}
+      text="settings"
+    />
   </Nav>
 );
 
 type SidebarItemProps = {
   icon: string, // actually an svg, but I dont know how to make a type for it
+  route: string,
   text: string
 };
 
-const ResponsiveSidebarItem = ({ icon, text }: SidebarItemProps) => (
-  <div className={styles.sidebarItem}>
-    <div className={styles.icon}>
-      <SVGInline svg={icon} className="icon" />
-    </div>
-    <div className={`${styles.text} d-sm-none d-md-block`}>{text}</div>
-  </div>
-);
+const ResponsiveSidebarItem = ({ icon, text, route }: SidebarItemProps) => {
+  const history = useHistory();
+  return (
+    <ClickableBox onClick={() => history.push(route)}>
+      <Nav.Item className={styles.sidebarItem}>
+        <div className={styles.icon}>
+          <SVGInline svg={icon} className="icon" />
+        </div>
+        <div className={`${styles.text} d-sm-none d-md-block`}>{text}</div>
+      </Nav.Item>
+    </ClickableBox>
+  );
+};
