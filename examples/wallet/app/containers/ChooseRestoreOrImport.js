c485172469afae54e16028129d5920b05047e476
Juan Pablo Capurro <jpcapurro@atixlabs.com>
2019-11-22 11:21:01 -0300

    Using redux actions to redirect to other pages in ChooseRestoreOrImport

---
+++ examples/wallet/app/containers/ChooseRestoreOrImport.js
@@ -0,0 +1,14 @@
+// @flow
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { push } from 'connected-react-router';
+import ChooseRestoreOrImport from '../pages/ChooseRestoreOrImport';
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ push }, dispatch);
+}
+
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(ChooseRestoreOrImport);
