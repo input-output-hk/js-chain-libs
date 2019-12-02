4f30f5833b59752090e2ed939b86a8760d1078ae
dmernies-ioh <diego.mernies@iohk.io>
2019-12-02 15:31:49 -0300

    Corrections by code review

--- examples/wallet/app/actions/router.js
+++ examples/wallet/app/actions/router.js
@@ -5,23 +5,15 @@ import type { Address } from '../models';
 import routes from '../constants/routes';
 import { loadAccountFromPrivateKey } from './account';
 
-import {
-  isAccountInfoInLocalStorage,
-  readAccountKeysFromLocalStorage
-} from '../utils/storage';
+import { readAccountKeysFromLocalStorage } from '../utils/storage';
 
 // eslint-disable-next-line import/prefer-default-export
 export const redirectToFirstAppPage = () => {
   return (dispatch: Dispatch, getState: GetState) => {
-    if (isAccountInfoInLocalStorage()) {
-      readAccountKeysFromLocalStorage()
-        .then(accountKeys => {
-          return loadAccountFromPrivateKey(accountKeys.privateKey);
-        })
-        .then(() => {
-          return dispatch(push(routes.WALLET));
-        })
-        .catch(error => console.error(error));
+    const accountKeys = readAccountKeysFromLocalStorage();
+    if (accountKeys !== undefined) {
+      loadAccountFromPrivateKey(accountKeys.privateKey);
+      return dispatch(push(routes.WALLET));
     }
     const {
       account: { address }
--- examples/wallet/app/utils/storage.js
+++ examples/wallet/app/utils/storage.js
@@ -1,25 +1,16 @@
-export const saveAccountInfoInLocalStorage = (keys: AccountKeys) => {
+export function saveAccountInfoInLocalStorage(keys: AccountKeys): void {
+  Object.keys(keys).forEach(key => localStorage.setItem(key, keys[key]));
   localStorage.setItem('savedKeys', 'true');
-  localStorage.setItem('address', keys.address);
-  localStorage.setItem('identifier', keys.identifier);
-  localStorage.setItem('privateKey', keys.privateKey);
-};
+}
 
-export const isAccountInfoInLocalStorage = () => {
+export function readAccountKeysFromLocalStorage(): any {
   const savedKeys = localStorage.getItem('savedKeys') === 'true';
-  return savedKeys;
-};
-
-export const readAccountKeysFromLocalStorage = () => {
-  return new Promise((resolve, reject) => {
-    const savedKeys = localStorage.getItem('savedKeys') === 'true';
-    if (savedKeys) {
-      const address = localStorage.getItem('address');
-      const identifier = localStorage.getItem('identifier');
-      const privateKey = localStorage.getItem('privateKey');
-      const accountKeys = { address, identifier, privateKey };
-      resolve(accountKeys);
-    }
-    reject(new Error('There are no keys stored in the wallet yet'));
-  });
-};
+  if (savedKeys) {
+    const address = localStorage.getItem('address');
+    const identifier = localStorage.getItem('identifier');
+    const privateKey = localStorage.getItem('privateKey');
+    const accountKeys = { address, identifier, privateKey };
+    return accountKeys;
+  }
+  return undefined;
+}
