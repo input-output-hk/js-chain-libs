d8c39cb5be98349d0d0f9c63f5cc708dd8467509
dmernies-ioh <diego.mernies@iohk.io>
2019-12-12 16:03:38 -0300

    A new component to revoer BIP39 phrases is added

--- examples/wallet/app/containers/RestoreWalletFromMnemonic.js
+++ examples/wallet/app/containers/RestoreWalletFromMnemonic.js
@@ -1,20 +1,14 @@
 // @flow
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import type { AppState } from '../reducers/types';
 import RestoreWalletFromMnemonic from '../components/RestoreWalletFromMnemonic';
-import { setAccountFromMnemonic } from '../actions/account';
-
-function mapStateToProps(state: AppState) {
-  const { unlockWalletPassword, isValidUnlockPassword } = state.account;
-  return { unlockWalletPassword, isValidUnlockPassword };
-}
+import { setAccount } from '../actions/account';
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ setAccountFromMnemonic }, dispatch);
+  return bindActionCreators({ setAccount }, dispatch);
 }
 
 export default connect(
-  mapStateToProps,
+  undefined,
   mapDispatchToProps
 )(RestoreWalletFromMnemonic);
