f4e53a93b9ced3a6995f808e63dca796feb72c42
dmernies-ioh <diego.mernies@iohk.io>
2020-01-08 14:58:28 -0300

    fake salt is removed

--- examples/wallet/app/utils/decrypt.js
+++ examples/wallet/app/utils/decrypt.js
@@ -3,14 +3,9 @@ import aesjs from 'aes-js';
 import blakejs from 'blakejs';
 
 const blake2b = data => blakejs.blake2b(data, null, 32);
-const SECRET = 'wallet_demo#';
-const AES_SECRET = ':aes_secret#';
 
 export function aesEncrypt(unlockWalletPassword: string, text: string): string {
-  const aesPasswordText = unlockWalletPassword
-    .concat(SECRET)
-    .concat(AES_SECRET);
-  const aesKey = blake2b(aesPasswordText);
+  const aesKey = blake2b(unlockWalletPassword);
   const aesCtr = new aesjs.ModeOfOperation.ctr(aesKey, new aesjs.Counter(5));
   const encryptedBytes = aesCtr.encrypt(aesjs.utils.utf8.toBytes(text));
   const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
@@ -21,10 +16,7 @@ export function aesDecrypt(
   unlockWalletPassword: string,
   encryptedHex: string
 ): string {
-  const aesPasswordText = unlockWalletPassword
-    .concat(SECRET)
-    .concat(AES_SECRET);
-  const aesKey = blake2b(aesPasswordText);
+  const aesKey = blake2b(unlockWalletPassword);
   const aesCtr = new aesjs.ModeOfOperation.ctr(aesKey, new aesjs.Counter(5));
   const decryptedBytes = aesCtr.decrypt(aesjs.utils.hex.toBytes(encryptedHex));
   const decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
